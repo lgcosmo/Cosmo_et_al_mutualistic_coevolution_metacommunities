0cdc6f688375b70923bf047199c1d225929e3167
DrWatson <no@mail>
2022-06-17 10:43:26 -0300

    File setup by DrWatson

---
+++ intro.jl
@@ -0,0 +1,16 @@
+using DrWatson
+@quickactivate "Cosmo_et_al_mutualistic_coevolution_metacommunities"
+
+println(
+"""
+Currently active project is: $(projectname())
+
+Path of active project: $(projectdir())
+
+Have fun with your new project!
+
+You can help us improve DrWatson by opening
+issues on GitHub, submitting feature requests,
+or even opening your own Pull Requests!
+"""
+)
